48db8e7614c96fb1a795edb585e66ccc54fcfb0e
Alex Umale <alexander.umale@gmail.com>
2025-02-17 22:49:13 -0500

    fix test

--- src/app/blogs/blogs.component.spec.ts
+++ src/app/blogs/blogs.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlogsComponent } from './blogs.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BlogsComponent', () => {
   let component: BlogsComponent;
@@ -8,10 +9,13 @@ describe('BlogsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BlogsComponent]
+      imports: [
+        BlogsComponent,
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(BlogsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
